a7aea3d03cdfe0be9cbab405cc0184784684e633
pineladsn <bruno.marques@redspark.io>
2023-07-27 10:03:32 -0300

    Very big sum problem and solution

---
+++ A Very Big Sum/very-big-sum.js
@@ -0,0 +1,17 @@
+"use strict";
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+function aVeryBigSum(ar) {
+  const sum = ar.reduce((acc, value) => {
+    return acc + value;
+  });
+  console.log(sum);
+  return sum;
+}
+
+aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]); //5000000015
