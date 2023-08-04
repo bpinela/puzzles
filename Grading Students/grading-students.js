01b85a1eab0386ef18e589a94276da83d7fca933
pineladsn <bruno.marques@redspark.io>
2023-08-03 21:58:46 -0300

    Add Gradient students problem

---
+++ Grading Students/grading-students.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades) {
+  // Write your code here
+}
