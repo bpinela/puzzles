e0e341bb7ca6dddb1e500307a6060049a6538935
pineladsn <bruno.marques@redspark.io>
2023-08-06 19:14:39 -0300

    Add anagram problem

---
+++ Anagram/anagram.js
@@ -0,0 +1,11 @@
+"use strict";
+/*
+ * Complete the 'anagram' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+function anagram(s) {
+  // Write your code here
+}
