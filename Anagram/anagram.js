cce6952d2a704abf957a603e7abac04c3b80c318
pineladsn <bruno.marques@redspark.io>
2023-08-07 17:18:19 -0300

    Add anagram solution

--- Anagram/anagram.js
+++ Anagram/anagram.js
@@ -6,6 +6,42 @@
  * The function accepts STRING s as parameter.
  */
 
+const compareArrays = (arr1, arr2) => {
+  return JSON.stringify(arr1) === JSON.stringify(arr2);
+};
+
 function anagram(s) {
-  // Write your code here
+  if (s.length % 2 === 1) {
+    console.log(-1);
+    return -1;
+  }
+
+  const partOne = Array.from(s.slice(0, s.length / 2)).sort();
+  const partTwo = Array.from(s.slice(s.length / 2)).sort();
+
+  if (compareArrays(partOne, partTwo)) {
+    console.log(0);
+    return 0;
+  }
+
+  partOne.map((val) => {
+    const idx = partTwo.indexOf(val);
+    if (idx !== -1) {
+      partTwo.splice(idx, 1);
+    }
+  });
+
+  console.log(partTwo.length);
+  return partTwo.length;
 }
+
+// ==== TESTS ====
+anagram("aaabbb"); //3
+anagram("ab"); //1
+anagram("abc"); //-1
+anagram("mnop"); //2
+anagram("xyyx"); //0
+anagram("xaxbbbxx"); //1
+anagram("asdfjoieufoa"); //3
+anagram("fdhlvosfpafhalll"); //5
+anagram("mvdalvkiopaufl"); //5
